1dd7714ca4224c5c6767fdbb11cc987531255a2f
heitorodrigue <160082563+heitorodrigue@users.noreply.github.com>
2025-04-17 13:28:07 -0300

    Add files via upload

---
+++ Idades.py
@@ -0,0 +1,9 @@
+print("Dados primeira pessoa: ")
+nome1 = input("Nome: ")
+idade1 = int(input("Idade: "))
+
+print("Dados segunda pessoa: ")
+nome2 = input("Nome: ")
+idade2 = int(input("idade: "))
+media= (idade1+idade2)/2
+print("A idade media de ",nome1," e de ", nome2, "é de ",media)
