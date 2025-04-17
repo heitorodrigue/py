8c57f77d7780543a7a502fc69b450b5a1de080e2
heitorodrigue <160082563+heitorodrigue@users.noreply.github.com>
2025-04-17 13:20:31 -0300

    Add files via upload

---
+++ Retangulo.py
@@ -0,0 +1,7 @@
+base = float(input("Base do retangulo: "));
+altura = float(input("Altura do retangulo: "))
+
+area = base * altura
+perimetro = (base*2)+(altura*2)
+print("Area: ",area)
+print("Perimetro: ",perimetro)
